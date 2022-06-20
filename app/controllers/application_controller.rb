522327b395af62f3e8dc05f0de1a72f1959c675c
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-20 20:13:46 +0530

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  protected 
+  protected
 
   def permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
