607ce7dc8bcfcdb3d44bd95c70a84d878deceaa8
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-20 20:08:40 +0530

    Add name field to user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(*)
+    new_user_session_path
+  end
+
+  protected 
+
+  def permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password])
+  end
 end
