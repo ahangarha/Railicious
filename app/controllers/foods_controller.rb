0660973ffa2e43c22088d55e3315982144f55c2c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-21 13:58:00 +0300

    add food#create functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,6 +5,14 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
+  def create
+    new_food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price))
+    new_food.user = current_user
+
+    new_food.save!
+    redirect_to foods_path
+  end
+
   def destroy
     food = Food.find(params[:id])
     return unless food.present? && food.user == current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :foods, only: [:index, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
