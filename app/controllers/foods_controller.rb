f00ef45579580057fedade081a0d8bc06094b64c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-21 13:11:47 +0300

    add food#delete method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,15 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @foods = Food.all
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+    return unless food.present? && food.user == current_user
+
+    food.destroy!
+    redirect_to foods_path
   end
 end
