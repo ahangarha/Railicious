edbb9c5c914b5d37cf3163903c67de96a3201992
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-22 23:06:32 +0300

    create and destory if user is the owner

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,10 +3,19 @@ class RecipeFoodsController < ApplicationController
 
   def create
     recipe = Recipe.find(params[:recipe_id])
-    new_recipe_food = RecipeFood.new(recipe_food_params)
-    new_recipe_food.recipe = recipe
+    if recipe.user == current_user
+      new_recipe_food = RecipeFood.new(recipe_food_params)
+      new_recipe_food.recipe = recipe
 
-    new_recipe_food.save!
+      if new_recipe_food.save!
+        flash[:notice] = 'Food added successfully.'
+      else
+        flash[:alert] = 'Failed to add food!'
+      end
+    else
+      flash[:alert] = 'Not authorized!'
+    end
+
     redirect_to recipe_path(recipe)
   end
 
@@ -14,16 +23,13 @@ class RecipeFoodsController < ApplicationController
     recipe_food = RecipeFood.includes(:recipe).find(params[:id])
 
     recipe = recipe_food.recipe
-    # food = recipe.foods.find(params[:id])
 
-    # puts '#'*80
-    # puts recipe_food
     if recipe_food.destroy!
       flash['notice'] = 'Food item removed from recipe successfully'
-      redirect_to recipe_path(recipe)
     else
-      flash.now['alert'] = 'Failed to delete the item!'
+      flash['alert'] = 'Failed to delete the item!'
     end
+    redirect_to recipe_path(recipe)
   end
 
   private
