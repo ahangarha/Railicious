0967e1df585693a3380b58b729a07cfd7a27d2d6
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-23 14:01:16 +0300

    fix more n+1 issues

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,6 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
 
   def self.public_recipes
-    @recipes = Recipe.includes(recipe_foods: [:food]).where(public: true).order(created_at: :desc)
+    @recipes = Recipe.includes(:user, recipe_foods: [:food]).where(public: true).order(created_at: :desc)
   end
 end
