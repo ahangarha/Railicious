fb0617a63550974a63f8122d0dc92d80394e0957
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-23 21:44:28 +0300

    create actual user_path and redirect from there to root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def show
+    redirect_to :root
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: :show
   resources :recipes, except: [:update] do
     resources :recipe_foods, only: [:create, :destroy]
   end
 
   resources :foods, only: [:index, :new, :create, :destroy]
-  get '/shopping_list', to: 'shopping_list#index' 
-
+  get '/shopping_list', to: 'shopping_list#index'
   # Defines the root path route ("/")
   get '/public_recipes', to: 'recipes#public'
   root to: "recipes#public"
