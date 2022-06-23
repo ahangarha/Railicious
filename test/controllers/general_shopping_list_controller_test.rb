7e92761091e4046b9408dbb9e34e1aa260fe02da
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-24 00:13:26 +0300

    user general_shopping_list instead of shopping_list

--- test/controllers/general_shopping_list_controller_test.rb
+++ test/controllers/general_shopping_list_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ShoppingListControllerTest < ActionDispatch::IntegrationTest
+class GeneralShoppingListControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
